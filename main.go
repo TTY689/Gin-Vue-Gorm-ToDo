69fb7cda69e89db2c9f55dfbd1459ed04ae90aed
czy <suoni1919@gmail.com>
2021-12-04 02:02:05 +0800

    💕后端部分基本进入测试环节

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"in_Vue_Gorm_ToDo/dao"
+	"in_Vue_Gorm_ToDo/models"
+	"in_Vue_Gorm_ToDo/routers"
 )
 
 func main() {
@@ -11,6 +13,16 @@ func main() {
 		fmt.Print("数据库初始化错误")
 		panic(err)
 	}
-
-	fmt.Println("数据库链接成功")
+	//fmt.Println("数据库链接成功")
+	err = dao.DB.AutoMigrate(&models.ToDo{})
+	if err != nil {
+		fmt.Print("数据库迁移失败")
+		return
+	}
+	r := routers.SetupRouter()
+	err = r.Run()
+	if err != nil {
+		fmt.Print("端口启动&运行服务失败")
+		return
+	}
 }
