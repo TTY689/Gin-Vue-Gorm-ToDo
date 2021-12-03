d5815dffb31c153ca46be474842de4ae311c443b
czy <suoni1919@gmail.com>
2021-12-04 01:57:01 +0800

    🥵路由层已完成

--- routers/routers.go
+++ routers/routers.go
@@ -1,6 +1,9 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"in_Vue_Gorm_ToDo/controller"
+)
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
@@ -10,6 +13,15 @@ func SetupRouter() *gin.Engine {
 	//设置模板文件路径
 	r.LoadHTMLGlob("templates/*")
 	//设置路由处理
-	r.GET("/")
+	r.GET("/", controller.IndexHandler)
+
+	v1Group := r.Group("v1")
+	{
+		v1Group.POST("/todo", controller.CreateToDo)
+		v1Group.GET("/todo", controller.GetToDoList)
+		v1Group.PUT("/todo", controller.UpdateAToDo)
+		v1Group.DELETE("/todo", controller.DeleteToDo)
+	}
+	return r
 
 }
