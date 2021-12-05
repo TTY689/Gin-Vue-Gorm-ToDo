12ea1f4747cda9777489543a309c127e3d0db777
czy <suoni1919@gmail.com>
2021-12-05 21:24:58 +0800

    🃏相关配置跟进

--- dao/mysql.go
+++ dao/mysql.go
@@ -1,11 +1,12 @@
 package dao
 
 import (
+	"fmt"
+	"gopkg.in/ini.v1"
+	//"github.com/go-ini/ini"
 	"gorm.io/driver/mysql"
 	_ "gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	//稍后版本将更新ini配置文件
-	//"github.com/go-ini/ini"
 )
 
 var (
@@ -13,15 +14,22 @@ var (
 )
 
 func InitMySQL() (err error) {
-	//ini配置待定
-	dsn := "czy:密码@tcp(8.136.224.253)/Go?charset=utf8mb4&parseTime=True&loc=Local"
+
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		fmt.Println("配置文件出错")
+		return err
+	}
+
+	username := cfg.Section("mysql").Key("username").String()
+	password := cfg.Section("mysql").Key("password").String()
+	ip := cfg.Section("mysql").Key("ip").String()
+	database := cfg.Section("mysql").Key("database").String()
+	var dsn string
+	dsn = username + ":" + password + "@tcp(" + ip + ")/" + database + "?charset=utf8mb4&parseTime=True&loc=Local"
+	//dsn := "czy:528429@tcp(8.136.224.253)/Go?charset=utf8mb4&parseTime=True&loc=Local"
 
 	DB, err = gorm.Open(mysql.New(mysql.Config{
 		DSN: dsn}), &gorm.Config{})
 	return DB.Error
 }
-
-//最新的Gorm已经删除了Close方法
-//func Close(){
-//	db.
-//}
